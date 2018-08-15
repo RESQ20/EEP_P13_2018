ee2573aa4dc442e2407ee2af868e3a9a24717369
notauniqueid <andy@hiro.org.uk>
2018-08-15 17:13:14 +0100

    'listchannels.py' is now a simple script to show which channels are available on your device

---
+++ PythonFiles/eep/listdevices.py
@@ -0,0 +1,44 @@
+import pyaudio
+import wave
+import argparse
+import os
+import time
+
+defaultframes = 512
+filecount = 1
+
+class textcolors:
+    blue = '\033[94m'
+    green = '\033[92m'
+    warning = '\033[93m'
+    fail = '\033[91m'
+    end = '\033[0m'
+
+recorded_frames = []
+device_info = {}
+useloopback = False
+recordtime = 5
+
+#Use module
+p = pyaudio.PyAudio()
+
+#Set default to first in list or ask Windows
+try:
+    default_device_index = p.get_default_input_device_info()
+except IOError:
+    default_device_index = -1
+
+#Select Device
+
+print (textcolors.blue + "Available devices:\n" + textcolors.end)
+for i in range(0, p.get_device_count()):
+    info = p.get_device_info_by_index(i)
+    print (str(info["index"]) + ": \t %s \t %s" % (info["name"], p.get_host_api_info_by_index(info["hostApi"])["name"]))
+
+    if default_device_index == -1:
+        default_device_index = info["index"]
+
+#Handle no devices available
+if default_device_index == -1:
+    print (textcolors.fail + "No device available" + textcolors.end)
+    exit()
